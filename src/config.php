564af2072616f0b78f9764ceaa63174a4fa7e43a
Muhsinun Chowdhury <muhsinun08@gmail.com>
2018-02-17 23:19:30 -0500

    update config.php

--- src/config.php
+++ src/config.php
@@ -1,7 +1,13 @@
 <?php
+// Rcon Settings
 $rconHost = "some.minecraftserver.com";
 $rconPort = 25575;
 $rconPassword = "server-rcon-password";
 $rconTimeout = 3;
+
+//Query Settings
 $queryPort = 25565;
+
+//Page Settings
+$pageLogo = "MCRcon";
 ?>
